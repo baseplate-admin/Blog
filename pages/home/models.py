d7a3ee2b9cc2f3e6c6a04397879ffdbd287de9d9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-10-27 23:05:27 +0600

    Update : Finalize Blog

--- pages/home/models.py
+++ pages/home/models.py
@@ -1,9 +1,18 @@
 from wagtail.core.models import Page
+from wagtailcache.cache import WagtailCacheMixin
+from django.db import models
+from wagtail.core.fields import StreamField
+
+from wagtail.admin.edit_handlers import (
+    StreamFieldPanel,
+)
+from custom.blocks import blocks
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 # Create your models here.
 
 
-class Home(Page):
+class Home(WagtailCacheMixin, Page):
     max_count = 1
     subpage_types = [
         "blog.BlogList",  # appname.ModelName
@@ -13,3 +22,22 @@ class Home(Page):
 
     class Meta:
         verbose_name = "Home"
+
+    author_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    
+    content = StreamField(
+        [
+            ("full_richtext", blocks.RichtextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+    content_panels = Page.content_panels + [
+        ImageChooserPanel("author_image"),
+        StreamFieldPanel("content"),
+    ]
